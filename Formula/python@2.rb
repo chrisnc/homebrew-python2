619e67e25d5e9866615baff45c191b3b0ac3d2f7
Chris Copeland <chris@chrisnc.net>
2021-05-11 09:16:55 -0700

    add big_sur bottle

--- Formula/python@2.rb
+++ Formula/python@2.rb
@@ -5,6 +5,10 @@ class PythonAT2 < Formula
   sha256 "b62c0e7937551d0cc02b8fd5cb0f544f9405bafc9a54d3808ed4594812edef43"
   head "https://github.com/python/cpython.git", branch: "2.7"
 
+  bottle do
+    sha256 big_sur: "bac348e5d0eadb79228d2c3d2529fb126fef1c11f807c8e1a89fbca1bbc71bc3"
+  end
+
   # setuptools remembers the build flags python is built with and uses them to
   # build packages later. Xcode-only systems need different flags.
   pour_bottle? do
