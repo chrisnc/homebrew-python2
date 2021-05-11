4888d35bff559e881d35572b91eeef6b93f09589
Chris Copeland <chris@chrisnc.net>
2021-05-11 06:26:09 -0700

    rename to python@2.rb

--- Formula/python@2.rb
+++ Formula/python@2.rb
@@ -1,4 +1,4 @@
-class PythonAT2718 < Formula
+class PythonAT2 < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tar.xz"
