2b3e8e2109f5d5a1173fbf82872106f596372155
Chris Copeland <chris@chrisnc.net>
2021-05-13 02:46:25 -0700

    remove redundant enable-framework configure option

--- Formula/python@2.rb
+++ Formula/python@2.rb
@@ -65,7 +65,6 @@ class PythonAT2 < Formula
       --enable-ipv6
       --datarootdir=#{share}
       --datadir=#{share}
-      --enable-framework=#{frameworks}
       --without-ensurepip
       --with-dbmliborder=gdbm:ndbm
       --enable-optimizations
